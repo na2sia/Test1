f01d856e9654593c7ccabd069d1963ffed85b41f
Natallia Abramchuk <na2sia88@mail.ru>
2015-04-16 15:26:21 +0400

    First commit

---
+++ Test1_Class.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class LinearFunction
+    {
+        private double a;
+        private double b;
+        
+        public double A
+        {
+            get {return a;}
+            set {a=value;}
+        }
+        
+        public double B
+        {
+            get {return b;}
+            set {b=value;}
+        }
+        
+        public double GetFunctionY(double x)
+        {
+            return (A * x) + B;
+
+        }
+    }
+}
---
+++ Test1_Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+
+    class Test1_Program
+    {
+        static void Main(string[] args)
+        {
+            LinearFunction LF = new LinearFunction();
+            LF.A=5;
+            LF.B=3;
+            Console.Write("X=");
+            double x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Y={0}",LF.GetFunctionY(x));
+            Console.ReadKey();
+
+            
+        }
+    }
+}
